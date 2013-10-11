765dd635361d55197387abd4fea82982248a065e
Jennifer Fong <jen@ednapiranha.com>
2013-10-10 18:30:15 -0700

    added font and whitelist

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var app = express();
 var server = require('http').createServer(app);
 var nconf = require('nconf');
 var settings = require('./settings')(app, configurations, express);
+var whitelist = require('./whitelist');
 
 nconf.argv().env().file({ file: 'local.json' });
 
 /* Filters for routes */
 
 var isLoggedIn = function(req, res, next) {
-  if (req.session.email) {
+  if (req.session.email && whitelist.indexOf(req.session.email) > -1) {
     next();
   } else {
     res.redirect('/');
